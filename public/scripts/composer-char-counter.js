97955e5185115bb42d16196935ace6a0a360700e
DashaDasha88 <dasha.kotova.mail@gmail.com>
2021-02-24 19:25:18 +0000

    add the code for counting down the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("textarea").on("input", function () {
+    const maxChars = 140
+    const remainingChars = maxChars - $(this).val().length;
+    const counter = $(this).closest(".new-tweet").find(".counter");
+    
+    counter.text(remainingChars);
+    if (remainingChars < 0) {
+      counter.css({"color": "red"});
+    } else {
+      counter.css({"color": "black"})
+    }
+    console.log(this);
+  })
+});
